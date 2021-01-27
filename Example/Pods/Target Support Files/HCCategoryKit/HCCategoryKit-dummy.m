c306c7e187f43e48e2edbc2fca80dd3e7998bf9f
niejiafa <niejiafa@haiercash.com>
2021-01-27 18:10:09 +0800

    add categorykit

---
+++ Example/Pods/Target Support Files/HCCategoryKit/HCCategoryKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HCCategoryKit : NSObject
+@end
+@implementation PodsDummy_HCCategoryKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-HCCategoryKit_Tests/Pods-HCCategoryKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HCCategoryKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_HCCategoryKit_Tests
+@end
